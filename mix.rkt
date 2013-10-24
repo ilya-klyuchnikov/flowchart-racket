ec7d42b3be05bb9e458cbac2f4da578555d2cb2a
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-10-24 17:39:15 +0400

    continuing to unify `mix` and `online`

--- mix.rkt
+++ mix.rkt
@@ -3,9 +3,10 @@
 (provide mix-prog)
 (require "parse.rkt" "eval.rkt" "pe.rkt" "util.rkt")
 
-;; this is very similar to online, but it accepts BTA!
-;; it assumes that s-vars are always s-vars
-;; (so it is a limited form of BTA, I guess)
+(define (make-store vars s-params s-vals d-params)
+  (hash-set-kv* (hash) s-params s-vals))
+
+
 
 ;; prog - a program for specialization
 ;; s-vars - a list of "static vars"
@@ -16,112 +17,106 @@
          [blocks          (program-blocks prog)]
          [vars            (collect-vars prog)]
          [d-params        (remove* s-vars params)]
-         [init-store      (hash-set-kv* (hash) s-vars s-vals)]
+         [init-store      (make-store vars s-vars s-vals d-params)]
          (init-state      (state (program-init-label prog) init-store))
          [_               (state->label-reset)]
          [blockmap        (hash-kv (map block-label blocks) blocks)]
-         [res-blocks      (mix-state init-state blockmap s-vars)])
+         [res-blocks      (pe-state init-state blockmap)])
     (program d-params (block-label (first res-blocks)) res-blocks)))
 
 ;; the main point
 ;; takes an initial state and a program (= blockmap)
 ;; and produces a list of residual blocks
 ;; the first block will be an initial one
-(define (mix-state init-state b-map s-vs)
-  (define (loop pending seen blocks)
-    (if (empty? pending) blocks
+(define (pe-state init-state b-map)
+  (define (loop pending seen res-blocks)
+    (if (empty? pending) res-blocks
         (match-let* ([(cons st pending) pending]
                      [(state lbl store)  st])          
           (let*-values 
               ([(bl)           (hash-ref b-map lbl)]
                [(res-label)    (state->label (state (block-label bl) store))]
-               [(states block) (mix-block bl store b-map '() res-label s-vs)]
+               [(states block) (pe-block bl store b-map '() res-label)]
                [(non-halts)    (filter-not halt-state? states)]
                [(seen)         (add-set st seen)]
                [(pending)      (remove* seen (add-pending* non-halts pending))])
-            (loop pending seen (cons block blocks))))))
+            (loop pending seen (cons block res-blocks))))))
   (reverse (loop (list init-state) '() '())))
 
 (define (compress? _) #f)
 
-;; IN  : in-block, s-store
+;; IN  : in-block, store
 ;; OUT : states, res-block
-(define (mix-block bl s-store b-map acc res-label s-vs)
+(define (pe-block bl store b-map acc res-label)
   (let*-values 
-      ([(next-store res-assigns) (mix-assigns (block-assigns bl) s-store s-vs)]
-       [(next-labels res-jump)  (online-jump (block-jump bl) next-store s-vs)])
+      ([(next-store res-assigns) (pe-assigns (block-assigns bl) store)]
+       [(next-labels res-jump)   (pe-jump (block-jump bl) next-store)])
     (match res-jump
       ; transition compression
-      [(goto (? compress? lb)) 
-       (mix-block (hash-ref b-map lb) next-store b-map (append acc res-assigns) res-label s-vs)]
+      [(goto (? compress? lb))
+       (pe-block (hash-ref b-map lb) next-store b-map (append acc res-assigns) res-label)]
       [_ (values (map (λ (l) (state l next-store)) next-labels)
                  (block res-label (append acc res-assigns) res-jump))])))
 
 ;; IN  : in-assigns,  store
 ;; OUT : out-assigns, store
-(define (mix-assigns assigns s-store s-vs)
-  (for/fold ([st s-store] [res-assigns '()]) ([asg assigns])    
-    (let-values ([(st delta) (mix-assign asg st s-vs)])
+(define (pe-assigns assigns store)
+  (for/fold ([st store] [res-assigns '()]) ([asg assigns])
+    (let-values ([(st delta) (pe-assign asg st)])
       (values st (append res-assigns delta)))))
 
 ;; IN  : in-assign,   sd-store
 ;; OUT : out-assigns, sd-store
 ;; out-assigns is either an empty list or one-element list
 ;; this is the big difference!
-;; if v is dynamic by BTA then then we perform assignmtent
-;; and do not propagate possible information!
-(define (mix-assign asg store s-vs)
+(define (pe-assign asg store)
   (match-let ([(assign v e) asg])
-    (if (member v s-vs)
+    (if (hash-has-key? store v)
         (values (hash-set store v (eval-exp e store)) '())
-        (values store (list (assign v (mix-exp e store s-vs)))))))
+        (values store (list (assign v (pe-exp e store)))))))
+
+
 
 ;; IN  : jump sd-store
 ;; OUT : labels, jump
 ;; where
 ;; * labels - a list of (original) labels in the residual jump
 ;; * jump - residual jump
-(define (online-jump jump s-store s-vs)
+(define (pe-jump jump store)
   (match jump
     [(goto label)
-     (define r-l (if (compress? label) label (state->label (state label s-store))))
+     (define r-l (if (compress? label) label (state->label (state label store))))
      (values (list label) (goto r-l))]
     [(return exp)      
-     (values (list (halt '())) (return (mix-exp exp s-store s-vs)))]
+     (values (list (halt '())) (return (pe-exp exp store)))]
     [(if-jump exp l1 l2)
-     (define r-l1 (state->label (state l1 s-store)))
-     (define r-l2 (state->label (state l2 s-store)))
-     (define g-l1 (if (compress? l1) l1 (state->label (state l1 s-store))))
-     (define g-l2 (if (compress? l2) l2 (state->label (state l2 s-store))))
-     (match (mix-exp exp s-store s-vs)
+     (define r-l1 (state->label (state l1 store)))
+     (define r-l2 (state->label (state l2 store)))
+     (define g-l1 (if (compress? l1) l1 (state->label (state l1 store))))
+     (define g-l2 (if (compress? l2) l2 (state->label (state l2 store))))
+     (match (pe-exp exp store)
        [(const e)
         (if e (values (list l1) (goto g-l1)) (values (list l2) (goto g-l2)))]
        [e
         (values (list l1 l2) (if-jump e r-l1 r-l2))])]))
 
-;; IN  :  an expression `exp` and an s-store
+;; IN  :  an expression `exp` and a store
 ;; OUT : a corresponding residual expression
 ;; the logic is that all sub-expressions are "residualized" recursively
-(define (mix-exp exp s-store s-vs)
+(define (pe-exp exp store)
   (match exp
     [(const datum) (const datum)]
-    [(varref var) (if (member var s-vs) (const (hash-ref s-store var)) (varref var))]
-    [(app op es) (mix-op op (map (λ (e) (mix-exp e s-store s-vs)) es))]
-    [_ (error "Unrecognized exp in mix-exp: " exp)]))
+    [(varref var)  (if (hash-has-key? store var) (const (hash-ref store var)) (varref var))]
+    [(app op es)   (pe-op op (map (λ (e) (pe-exp e store)) es))]
+    [_ (error "Unrecognized exp in pe-exp: " exp)]))
 
 ;; IN  : an operations `op` and its arguments `pe-vals`
 ;; OUT : a residual expression
-;; if all args are const, evaluated, otherwise, - residualized
-(define (mix-op op pe-vals)
+;; if all args are static, evaluated, otherwise, - residualized
+(define (pe-op op pe-vals)
   (if (andmap const? pe-vals)
       (const (eval-op op (map pe-val->object pe-vals)))
       (app op pe-vals)))
-
-;; pe-val -> exp     
-#;(define (lift pe-val)
-    (match pe-val
-      [(static obj) (const obj)]
-      [(dynamic obj) obj]))
 
 (define (pe-val->object pe-val)
   (match pe-val
--- online.rkt
+++ online.rkt
@@ -4,7 +4,7 @@
 (require "parse.rkt" "eval.rkt" "pe.rkt" "util.rkt")
 
 (define (make-store vars s-params s-vals d-params)
-  (define s 
+  (define s
     (hash-set-kv* (hash-init vars (static '())) s-params (map static s-vals)))
   (hash-set-kv* s d-params (map (λ (v) (dynamic (varref v))) d-params)))
 
@@ -21,14 +21,14 @@
          (init-state      (state (program-init-label prog) init-store))
          [_               (state->label-reset)]
          [blockmap        (hash-kv (map block-label blocks) blocks)]
-         [res-blocks      (online-state init-state blockmap)])
+         [res-blocks      (pe-state init-state blockmap)])
     (program d-params (block-label (first res-blocks)) res-blocks)))
 
 ;; the main point
 ;; takes an initial state and a program (= blockmap)
 ;; and produces a list of residual blocks
 ;; the first block will be an initial one
-(define (online-state init-state b-map)
+(define (pe-state init-state b-map)
   (define (loop pending seen res-blocks)
     (if (empty? pending) res-blocks
         (match-let* ([(cons st pending) pending]
@@ -36,36 +36,42 @@
           (let*-values 
               ([(bl)           (hash-ref b-map lbl)]
                [(res-label)    (state->label (state (block-label bl) store))]
-               [(states block) (online-block bl store res-label)]
-               [(non-halts)    (filter-not halt-state? states)]                   
+               [(states block) (pe-block bl store b-map '() res-label)]
+               [(non-halts)    (filter-not halt-state? states)]
                [(seen)         (add-set st seen)]
                [(pending)      (remove* seen (add-pending* non-halts pending))])
             (loop pending seen (cons block res-blocks))))))
   (reverse (loop (list init-state) '() '())))
 
-;; IN  : in-block, sd-store
+(define (compress? _) #f)
+
+;; IN  : in-block, store
 ;; OUT : states, res-block
-;; states (list of new states) (one or two -- depending on a jump)
-(define (online-block bl sd-store res-label)
+(define (pe-block bl store b-map acc res-label)
   (let*-values 
-      ([(next-store res-assigns) (online-assigns (block-assigns bl) sd-store)]
-       [(next-labels res-jump)   (online-jump (block-jump bl) next-store)])
-    (values (map (λ (l) (state l next-store)) next-labels)
-            (block res-label res-assigns res-jump))))
+      ([(next-store res-assigns) (pe-assigns (block-assigns bl) store)]
+       [(next-labels res-jump)   (pe-jump (block-jump bl) next-store)])
+    (match res-jump
+      ; transition compression
+      [(goto (? compress? lb))
+       (pe-block (hash-ref b-map lb) next-store b-map (append acc res-assigns) res-label)]
+      [_ (values (map (λ (l) (state l next-store)) next-labels)
+                 (block res-label (append acc res-assigns) res-jump))])))
 
 ;; IN  : in-assigns,  store
 ;; OUT : out-assigns, store
-(define (online-assigns assigns sd-store)
-  (for/fold ([st sd-store] [res-assigns '()]) ([asg assigns])    
-    (let-values ([(st delta) (online-assign asg st)])
+(define (pe-assigns assigns store)
+  (for/fold ([st store] [res-assigns '()]) ([asg assigns])
+    (let-values ([(st delta) (pe-assign asg st)])
       (values st (append res-assigns delta)))))
 
 ;; IN  : in-assign,   sd-store
 ;; OUT : out-assigns, sd-store
 ;; out-assigns is either an empty list or one-element list
-(define (online-assign asg store)
+;; this is the big difference!
+(define (pe-assign asg store)
   (match-let ([(assign v e) asg])
-    (match (online-exp e store)
+    (match (pe-exp e store)
       [(static obj)
        (values (hash-set store v (static obj)) '())]
       [(dynamic obj)
@@ -76,49 +82,48 @@
 ;; where
 ;; * labels - a list of (original) labels in the residual jump
 ;; * jump - residual jump
-(define (online-jump jump sd-store)
+(define (pe-jump jump store)
   (match jump
     [(goto label)
-     (define r-l (state->label (state label sd-store)))
+     (define r-l (if (compress? label) label (state->label (state label store))))
      (values (list label) (goto r-l))]
     [(return exp)      
-     (values (list (halt '())) (return (lift (online-exp exp sd-store))))]
+     (values (list (halt '())) (return (lift (pe-exp exp store))))]
     [(if-jump exp l1 l2)
-     (define r-l1 (state->label (state l1 sd-store)))
-     (define r-l2 (state->label (state l2 sd-store)))
-     (define g-l1 (state->label (state l1 sd-store)))
-     (define g-l2 (state->label (state l2 sd-store)))
-     (match (online-exp exp sd-store)
+     (define r-l1 (state->label (state l1 store)))
+     (define r-l2 (state->label (state l2 store)))
+     (define g-l1 (if (compress? l1) l1 (state->label (state l1 store))))
+     (define g-l2 (if (compress? l2) l2 (state->label (state l2 store))))
+     (match (pe-exp exp store)
        [(static e)
         (if e (values (list l1) (goto g-l1)) (values (list l2) (goto g-l2)))]
-       [(dynamic e)        
+       [(dynamic e)
         (values (list l1 l2) (if-jump e r-l1 r-l2))])]))
 
-;; IN  :  an expression `exp` and a sd-store
+;; IN  :  an expression `exp` and a store
 ;; OUT : a corresponding residual expression
 ;; the logic is that all sub-expressions are "residualized" recursively
-(define (online-exp exp sd-store)
+(define (pe-exp exp store)
   (match exp
     [(const datum) (static datum)]
-    [(varref var)  (hash-ref sd-store var)]
-    [(app op es) (online-op op (map (λ (e) (online-exp e sd-store)) es))]
-    [_ (error "Unrecognized exp in online-exp: " exp)]))
+    [(varref var)  (hash-ref store var)]
+    [(app op es)   (pe-op op (map (λ (e) (pe-exp e store)) es))]
+    [_ (error "Unrecognized exp in pe-exp: " exp)]))
 
 ;; IN  : an operations `op` and its arguments `pe-vals`
 ;; OUT : a residual expression
 ;; if all args are static, evaluated, otherwise, - residualized
-(define (online-op op pe-vals)
+(define (pe-op op pe-vals)
   (if (andmap static? pe-vals)
       (static (eval-op op (map pe-val->object pe-vals)))
       (dynamic (app op (map lift pe-vals)))))
 
-;; pe-val -> exp     
-(define (lift pe-val)
-  (match pe-val
-    [(static obj) (const obj)]
-    [(dynamic obj) obj]))
-
 (define (pe-val->object pe-val)
   (match pe-val
     [(static obj) obj]
+    [(dynamic obj) obj]))
+
+(define (lift pe-val)
+  (match pe-val
+    [(static obj) (const obj)]
     [(dynamic obj) obj]))
